5657d6d01511ea93966d49157280f569395a532e
Abdul Quadir <60433139+Abdul5@users.noreply.github.com>
2022-11-18 02:52:05 +0530

    Add files via upload

---
+++ LastOccuranceIndex.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class LastOccuranceIndex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0;i < n;i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        for(int i = n-1; i >= 0;i--) {
+            if(arr[i] == k) {
+                System.out.print(i);
+                return;
+            }
+        }
+        System.out.print("-1");
+
+	}
+
+}
