3912a48b0bc2fa37df7dcd3b9cc276570918cc8e
Abdul Quadir <60433139+Abdul5@users.noreply.github.com>
2022-12-08 16:00:19 +0530

    Add files via upload

---
+++ PrintArrayUsingRec.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class PrintArrayUsingRec {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        int[] arr = new int[n];
+
+	        for(int i = 0; i < n; i++)
+	            arr[i] = sc.nextInt();
+	        PrintArray(arr, 0);
+	    }
+
+		public static void PrintArray(int[] arr, int n) {
+	      // Write your code here
+			if(n == arr.length) return;
+			System.out.print(arr[n]+" ");
+			PrintArray(arr,n + 1);
+	    }
+
+}
