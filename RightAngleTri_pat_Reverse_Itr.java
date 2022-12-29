ee3f73b70746c6af6e67e7d2d2bab86df62d4107
Abdul Quadir <60433139+Abdul5@users.noreply.github.com>
2022-12-30 01:02:49 +0530

    Add files via upload

---
+++ RightAngleTri_pat_Reverse_Itr.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class RightAngleTri_pat_Reverse_Itr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 1;i <= n;i++) {
+			for(int j = i;j >= 1;j--) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
