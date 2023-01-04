9cc09b4a44c456fe423ab2ba61a7d03d9c8766d9
Abdul Quadir <60433139+Abdul5@users.noreply.github.com>
2023-01-05 03:09:17 +0530

    Add files via upload

---
+++ Print_X_Shape.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Print_X_Shape {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+	    int n = scn.nextInt();
+	      for(int i=1;i<=n;i++){
+	         for(int j=1;j<=n;j++){
+	            if(i == j || i + j == n + 1) {
+					System.out.print("*");
+				}
+	            else {
+					System.out.print(" ");
+				}
+	         }
+	         System.out.println();
+	       }
+
+	}
+
+}
