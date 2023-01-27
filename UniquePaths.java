265fc6820f5d6410546bb471f48fb88ecd5f24a2
Abdul Quadir <60433139+Abdul5@users.noreply.github.com>
2023-01-28 03:09:20 +0530

    Add files via upload

---
+++ UniquePaths.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class UniquePaths {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int m = sc.nextInt();
+	        int n = sc.nextInt();
+	        sc.close();
+
+	        int[][] dp = new int[m][n];
+
+	        for (int i = 0; i < m; i++) {
+	            for (int j = 0; j < n; j++) {
+	                if (i == 0 || j == 0) {
+	                    dp[i][j] = 1;
+	                } else {
+	                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+	                }
+	            }
+	        }
+
+	        System.out.println(dp[m - 1][n - 1]);
+	    }
+
+}
