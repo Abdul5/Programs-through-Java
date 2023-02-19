ef9ce6a66026d3917f84b8c14d761bd17118112c
Abdul Quadir <60433139+Abdul5@users.noreply.github.com>
2023-02-20 04:40:25 +0530

    Add files via upload

---
+++ UsingFloat.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class UsingFloat {
+	    public static void main(String args[]) {
+	        // your code here
+			float f= 10.45f;
+			f = 20.55f;
+			System.out.println(f);
+	    }
+}
