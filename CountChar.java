c96575dfbb342f0d4b128ce7f70846bed36257e3
Abdul Quadir <60433139+Abdul5@users.noreply.github.com>
2023-03-06 23:45:04 +0530

    Add files via upload

---
+++ CountChar.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class CountChar {
+		  public static void main(String[] args) {
+			  Scanner sc = new Scanner(System.in);
+
+		    // create a string
+		    String greet = sc.nextLine();
+		    System.out.println("String: " + greet);
+
+		    // get the length of greet
+		    int length = greet.length();
+		    System.out.println("Length: " + length);
+		  }
+
+}
