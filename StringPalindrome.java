2a296960dd93d92efa47f4969963e4adf473acff
Abdul Quadir <60433139+Abdul5@users.noreply.github.com>
2023-07-16 04:06:29 +0530

    Update StringPalindrome.java

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -21,3 +21,26 @@ public class StringPalindrome {
 	}
 
 }
+
+
+
+
+import java.util.*;
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String str1 = "";
+        int len = str.length();
+        for(int i = 0;i < len;i++) {
+            char ch = str.charAt(i);
+            str1 = ch + str1;
+        }
+        if(str.equalsIgnoreCase(str1)) {
+            System.out.println("is palindrome");
+        }
+        else {
+            System.out.println("not palindrome");
+        }
+    }
+}
